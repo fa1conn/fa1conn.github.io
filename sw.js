3aee5589410335c0397d55b6594a04e48fae49ed
fa1conn <fa1concyf0@gmail.com>
2019-11-20 12:07:58 +0800

    new version

--- sw.js
+++ sw.js
@@ -24,7 +24,6 @@ const PRECACHE_LIST = [
   "./img/home-bg.jpg",
   "./img/404-bg.jpg",
   "./css/hux-blog.min.css",
-  "./css/syntax.css",
   "./css/bootstrap.min.css"
   // "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css",
   // "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/fonts/fontawesome-webfont.woff2?v=4.6.3",
@@ -32,8 +31,8 @@ const PRECACHE_LIST = [
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "fa1conn.github.io",
-  "fa1conn.io",
+  "huangxuan.me",
+  "yanshuo.io",
   "cdnjs.cloudflare.com"
 ]
 const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
